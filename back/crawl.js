b90eca3421ddde96ad375f7f7aee8c0f81d5242a
Hugo Knorr <hugo.knorr@gmail.com>
2019-06-30 16:57:11 +0200

    Improve crawler

--- back/crawl.js
+++ back/crawl.js
@@ -3,7 +3,7 @@ const { Client } = require('es6');
 const client = new Client({ node: 'http://localhost:9201' });
 
 const bulkData = data =>
-  data.filter(datum => datum.rating.average !== null).reduce(
+  data.reduce(
     (acc, item) =>
       acc.concat([
         {
@@ -18,28 +18,86 @@ const bulkData = data =>
     []
   );
 
+const bulkFormat = (data, id) =>
+  data.reduce((acc, item) => {
+    item.tv_show_id = id;
+    return acc.concat([
+      {
+        index: {
+          _index: 'tv_shows_episodes',
+          _type: '_doc',
+          _id: item.id
+        }
+      },
+      item
+    ]);
+  }, []);
+
+const indexTvShowsContent = show => {
+  return new Promise(resolve => {
+    const crawl = () =>
+      axios
+        .get(`https://api.tvmaze.com/shows/${show.id}/episodes`)
+        .then(resp => {
+          if (resp.data.length === 0) resolve();
+          client
+            .bulk({
+              refresh: true,
+              body: bulkFormat(resp.data, show.id)
+            })
+            .then(() => {
+              console.log(`[${show.id}] ${show.name} episodes indexed!`);
+              resolve();
+            });
+        });
+    crawl().catch(e => {
+      if (e.response.status === 429) {
+        console.log('WARNING !!! Rate limit !!!!');
+        setTimeout(crawl, 5 * 1000);
+      } else {
+        console.log(e);
+        resolve();
+      }
+    });
+  });
+};
+
+const bulkIndexTvShowsContent = async shows => {
+  for (let index = 0; index < shows.length; index++) {
+    const show = shows[index];
+    await indexTvShowsContent(show);
+  }
+};
+
 const indexTvShows = (page = 0) => {
   axios
     .get(`https://api.tvmaze.com/shows?page=${page}`)
-    .then(resp => {
-      indexTvShows(page + 1);
+    .then(({ data }) => {
+      const shows = data.filter(show => show.rating.average !== null);
+      console.log(shows.length);
 
-      const bulk = bulkData(resp.data);
-      debugger;
-
-      client
-        .bulk({
-          refresh: true,
-          body: bulk
-        })
-        .then(() => {
-          console.log(`Page ${page} indexed!`);
-        });
+      bulkIndexTvShowsContent(shows).then(() => {
+        client
+          .bulk({
+            refresh: true,
+            body: bulkData(shows)
+          })
+          .then(() => {
+            console.log(`Page ${page} of shows indexed!`);
+            console.log(
+              '----------------- Next Page ! ------------------------'
+            );
+            indexTvShows(page + 1);
+          });
+      });
     })
     .catch(e => {
-      console.error(e);
+      if (e.response && e.response.status === 429) {
+        console.log('WARNING !!! Rate limit !!!!');
+        setTimeout(() => indexTvShows(page), 5 * 1000);
+      }
       console.log(`${page - 1} pages crawled! Over...`);
     });
 };
 
-indexTvShows();
+indexTvShows(171);
